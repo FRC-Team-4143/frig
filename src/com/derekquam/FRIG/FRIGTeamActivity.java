6bb2c0d88e8182d82c2ea0546c5117c82b8b0f2a
dquam <dquam@924e1869-c540-46d6-93b1-acf53c8139ee>
2012-04-19 04:07:36 +0000

    Adding image resize and upload functions

--- src/com/derekquam/FRIG/FRIGTeamActivity.java
+++ src/com/derekquam/FRIG/FRIGTeamActivity.java
@@ -1,9 +1,19 @@
 package com.derekquam.FRIG;
 
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Matrix;
 import android.net.Uri;
 import android.os.Bundle;
 import android.os.Environment;
@@ -71,6 +81,9 @@ public class FRIGTeamActivity extends Activity {
 		super.onActivityResult(requestCode, resultCode, data);
 		if (requestCode == CAPTURE_IMAGE_ACTIVITY_REQUEST_CODE) {
 			if (resultCode == RESULT_OK) {
+				String outPath = fileUri.getPath().replace(".jpg", "_small.jpg");
+				ResizePicture(fileUri.getPath(), 800, outPath);
+				UploadPicture(outPath);
 				// Image captured and saved to fileUri specified in the Intent
 				Toast.makeText(this, "Image saved to:\n" +
 						fileUri, Toast.LENGTH_LONG).show();
@@ -79,6 +92,128 @@ public class FRIGTeamActivity extends Activity {
 			} else {
 				// Image capture failed, advise user
 			}
+		}
+	}
+
+	protected void ResizePicture(String imagePath, int desiredWidth, String imageOutPath) {
+		// Get the source image's dimensions
+		BitmapFactory.Options options = new BitmapFactory.Options();
+		options.inJustDecodeBounds = true;
+		BitmapFactory.decodeFile(imagePath, options);
+
+		int srcWidth = options.outWidth;
+		int srcHeight = options.outHeight;
+
+		// Only scale if the source is big enough. This code is just trying to fit a image into a certain width.
+		if(desiredWidth > srcWidth)
+			desiredWidth = srcWidth;
+
+
+
+		// Calculate the correct inSampleSize/scale value. This helps reduce memory use. It should be a power of 2
+		// from: http://stackoverflow.com/questions/477572/android-strange-out-of-memory-issue/823966#823966
+		int inSampleSize = 1;
+		while(srcWidth / 2 > desiredWidth) {
+			srcWidth /= 2;
+			srcHeight /= 2;
+			inSampleSize *= 2;
+		}
+
+		float desiredScale = (float) desiredWidth / srcWidth;
+
+		// Decode with inSampleSize
+		options.inJustDecodeBounds = false;
+		options.inDither = false;
+		options.inSampleSize = inSampleSize;
+		options.inScaled = false;
+		options.inPreferredConfig = Bitmap.Config.ARGB_8888;
+		Bitmap sampledSrcBitmap = BitmapFactory.decodeFile(imagePath, options);
+
+		// Resize
+		Matrix matrix = new Matrix();
+		matrix.postScale(desiredScale, desiredScale);
+		Bitmap scaledBitmap = Bitmap.createBitmap(sampledSrcBitmap, 0, 0, sampledSrcBitmap.getWidth(), sampledSrcBitmap.getHeight(), matrix, true);
+		sampledSrcBitmap = null;
+
+		// Save
+		FileOutputStream out;
+		try {
+			out = new FileOutputStream(imageOutPath);
+			scaledBitmap.compress(Bitmap.CompressFormat.JPEG, 100, out);
+			scaledBitmap = null;
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+
+	protected void UploadPicture(String imagePath) {
+		HttpURLConnection connection = null;
+		DataOutputStream outputStream = null;
+
+		String urlServer = "http://www.derekquam.com/handle_upload.php";
+		String lineEnd = "\r\n";
+		String twoHyphens = "--";
+		String boundary =  "*****";
+
+		int bytesRead, bytesAvailable, bufferSize;
+		byte[] buffer;
+		int maxBufferSize = 1*1024*1024;
+
+		try
+		{
+			FileInputStream fileInputStream = new FileInputStream(new File(imagePath) );
+
+			URL url = new URL(urlServer);
+			connection = (HttpURLConnection) url.openConnection();
+
+			// Allow Inputs & Outputs
+			connection.setDoInput(true);
+			connection.setDoOutput(true);
+			connection.setUseCaches(false);
+
+			// Enable POST method
+			connection.setRequestMethod("POST");
+
+			connection.setRequestProperty("Connection", "Keep-Alive");
+			connection.setRequestProperty("Content-Type", "multipart/form-data;boundary="+boundary);
+
+			outputStream = new DataOutputStream( connection.getOutputStream() );
+			outputStream.writeBytes(twoHyphens + boundary + lineEnd);
+			outputStream.writeBytes("Content-Disposition: form-data; name=\"uploadedfile\";filename=\"" + imagePath +"\"" + lineEnd);
+			outputStream.writeBytes(lineEnd);
+
+			bytesAvailable = fileInputStream.available();
+			bufferSize = Math.min(bytesAvailable, maxBufferSize);
+			buffer = new byte[bufferSize];
+
+			// Read file
+			bytesRead = fileInputStream.read(buffer, 0, bufferSize);
+
+			while (bytesRead > 0)
+			{
+				outputStream.write(buffer, 0, bufferSize);
+				bytesAvailable = fileInputStream.available();
+				bufferSize = Math.min(bytesAvailable, maxBufferSize);
+				bytesRead = fileInputStream.read(buffer, 0, bufferSize);
+			}
+
+			outputStream.writeBytes(lineEnd);
+			outputStream.writeBytes(twoHyphens + boundary + twoHyphens + lineEnd);
+
+			// Responses from the server (code and message)
+			int serverResponseCode = connection.getResponseCode();
+			String serverResponseMessage = connection.getResponseMessage();
+			
+			Toast.makeText(this, serverResponseMessage, Toast.LENGTH_LONG).show();
+
+			fileInputStream.close();
+			outputStream.flush();
+			outputStream.close();
+		}
+		catch (Exception ex)
+		{
+			//Exception handling
 		}
 	}
 }
